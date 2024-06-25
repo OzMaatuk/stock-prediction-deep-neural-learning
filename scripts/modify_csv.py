8d9a800904fd2e62c8337e1a5296e540bc438009
Oz <ozmaatuk@gmail.com>
2024-06-25 04:52:40 +0000

    new usage

--- scripts/modify_csv.py
+++ scripts/modify_csv.py
@@ -18,16 +18,19 @@ import os
 # entries = os.listdir(DIR_PATH)
 # files = [f for f in entries if os.path.isfile(os.path.join(DIR_PATH, f))]
 
-# # remove column
-# for file_path in files:
-#     relative_file_path = DIR_PATH + file_path
-#     print(relative_file_path)    
-#     col_name = "Unnamed: 0.1"
-#     data = pd.read_csv(relative_file_path)
+
+# files = ["GOOG_COMB.csv", "GSPC_COMB.csv", "HOOD_COMB.csv", "JMIA_COMB.csv", "TSEM_COMB.csv"]
+
+# remove column
+# for file in files:
+#     file_path = DIR_PATH + file
+#     print(file_path)    
+#     col_name = "Unnamed: 0"
+#     data = pd.read_csv(file_path, index_col=0)
 #     cols = data.columns
 #     if col_name in cols:
 #         data.drop(columns=[col_name] ,inplace=True)
-#         data.to_csv(relative_file_path)
+#         data.to_csv(file_path)
 
 
 # remove index column from csv
@@ -41,18 +44,20 @@ import os
 
 
 
-# files = ["GOOG_1.csv", "GOOG_2.csv", "GOOG_3.csv"]
+# files = ["GOOG.csv", "GSPC.csv", "HOOD.csv", "JMIA.csv", "TSEM.csv", "TSLA.csv"]
 
 
 # # combine csv files
 # l = []
-# for file_path in files:
-#     relative_file_path = DIR_PATH + file_path
+# for file in files:
+#     relative_file_path = DIR_PATH + file
 #     l.append(pd.read_csv(relative_file_path))
-# combined_df = pd.concat(l, ignore_index=True)
-# output_fie_path = DIR_PATH + "GOOG_COMB.csv"
-# combined_df.to_csv(output_fie_path, index=True)
-# print(f"CSV files appended and saved to: {output_fie_path}")
+#     new_file_relaative_path = DIR_PATH + file[:-4] + '_4.csv'
+#     l.append(pd.read_csv(new_file_relaative_path))
+#     combined_df = pd.concat(l, ignore_index=True)
+#     output_fie_path = DIR_PATH + file[:-4] + "_COMB.csv"
+#     combined_df.to_csv(output_fie_path, index=True)
+#     print(f"CSV files appended and saved to: {output_fie_path}")
 
 
 # combine min data and day data
