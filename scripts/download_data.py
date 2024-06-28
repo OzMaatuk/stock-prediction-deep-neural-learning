2d2c6e74728f345cdc662932e2e36242cd977173
Oz <ozmaatuk@gmail.com>
2024-06-28 06:18:29 +0000

    downloading latest data and adding nvidia

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -2,16 +2,15 @@ import pandas as pd
 import yfinance as yf
 
 # show data for different tickers
-start = pd.to_datetime('2024-06-07')
+start = pd.to_datetime('2024-06-20')
 end = pd.to_datetime('2024-06-27')
 
 
-# stocks = ['^GSPC','GOOG','TSLA','HOOD','JMIA','TSEM']
-stocks = ['GOOG']
+stocks = ['^GSPC','GOOG','TSLA','HOOD','JMIA','TSEM', 'NVDA']
 
 for stock in stocks:
-    data = yf.download(stock, start=start, end=end)
-    # data = yf.download(stock, start=start, end=end, interval="1m")
+    # data = yf.download(stock, start=start, end=end)
+    data = yf.download(stock, start=start, end=end, interval="1m")
     # print(data)
-    data.to_csv(stock + '_unseen.csv')
+    data.to_csv(stock + '_5.csv')
 
